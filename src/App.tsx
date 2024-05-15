9d32afc25352da6d34079cbddea322576873062c
dibeshkc7 <dibeshkc7.com>
2024-05-15 11:59:59 +0545

    display

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Header from './layout/Header';
+import Footer from './layout/Footer';
+
+import Display from './component/Display';
+
+
 
 function App() {
+
+  const handleClick = () => {
+    console.log("Clicked")
+  }
+  const handleClick2 = () => {
+    console.log("I am clicked")
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <Display
+        FirstName={"Alesh"}
+        LastName={"Maharjan"}
+        address={"aaa"}
+        age={50}
+        onClick={handleClick}
+      />
+      <Display
+        FirstName={"aaaa"}
+        LastName={"Maharjan"}
+        address={"aaa"}
+        age={50}
+        onClick={handleClick2}
+      />
+      <Footer />
     </div>
+
   );
 }
 
