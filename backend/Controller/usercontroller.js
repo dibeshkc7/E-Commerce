18521e9a585f669f7bac0081f552614eda74b083
dibeshkc7 <dibeshkc7.com>
2024-07-24 19:27:14 +0545

    login and password

---
+++ backend/Controller/usercontroller.js
@@ -0,0 +1,74 @@
+const { default: userModel } = require("../Model/userModel");
+const bcrypt = require("bcrypt")
+const UserModel = require("../Model/userModel");
+
+exports.CreateUser = async (req, res) => {
+  const checkEmail = await UserModel.findOne({ email: req.body.email });
+  const checkContact = await UserModel.findOne({
+    "userDetail.phoneNumber": req.body.phoneNumber,
+  });
+
+  if (checkContact) {
+    return res.status(400).json({ error: "Contact already exi st" });
+  }
+  if (checkEmail) {
+    return res.status(400).json({ error: "Email already exist" });
+  } else {
+    const hashPassword = await bcrypt.hash(req.body.password, 10);
+
+    const CreateUser = await new UserModel({
+      email: req.body.email,
+      password: hashPassword,
+      "userDetail.firstName": req.body.firstName,
+      "userDetail.middleName": req.body.middleName,
+      "userDetail.lastName": req.body.lastName,
+      "userDetail.phoneNumber": req.body.phoneNumber,
+    });
+
+    const saveUser = await CreateUser.save();
+    if (!saveUser) {
+      return res.status(400).json({ error: "User registration failed" });
+    } else {
+      return res.status(400).json({ message: "User registration succesful" });
+    }
+  }
+};
+
+exports.getAllUser = async (req, res) => {
+    const users = await UserModel.find();
+    if (!users) {
+        return res.status(400).json({ message: "Users not found" })
+    }
+    return res.send(users)
+}
+
+exports.updateUser = async (req,res)=>{
+    const update = await UserModel.findByIdAndUpdate(req.params.id, {
+      "userDetail.firstName": req.body.firstName,
+      "userDetail.middleName": req.body.middleName,
+      "userDetail.lastName": req.body.lastName,
+      "userDetail.gender": req.body.gender,
+      "userDetail.address": req.body.address,   
+    }, { new: true })
+
+    if (!update) {
+        return res.json({ message: "Not Found" }).status(400);
+    }
+    res.send(update);
+}
+
+exports.logIn = async (req, res) => {
+  const { email, password } = req.body;
+
+  const checkUser = await UserModel.findOne({ email: email });
+
+  if (!checkUser) {
+    return res.json({ error: "User not found" }).status(400);
+  }
+  const checkPassword = await bcrypt.compare(password, checkUser.password);
+
+  if (!checkPassword) {
+    return res.json({ error: "Password is invalid" }).status(400);
+  }
+  return res.json({ message: "Login Successful" }).status(201);
+}
---
+++ backend/Route/userRoute.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const { CreateUser, getAllUser, updateUser, logIn } = require("../Controller/usercontroller");
+
+const router = express.Router();
+
+router.post('/register', CreateUser);
+router.get('/users', getAllUser);
+router.put('/update-user/:id', updateUser);
+router.post('/login', logIn);
+
+module.exports = router;
+
